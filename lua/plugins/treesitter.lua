c7b3665193ddd6bafe81545702a6d42da7345b9c
Leo Liu <liuleo184@g.ucla.edu>
2026-03-06 06:56:54 +0800

    treesitter textobject added

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,15 +1,62 @@
 return {
-  "MeanderingProgrammer/treesitter-modules.nvim",
-  dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-  },
-  ---@module 'treesitter-modules'
-  ---@type ts.mod.UserConfig
-  opts = {
-    ensure_installed = { "lua", },
-    auto_install = true,
-    highlight = {
-      enable = true,
+  {
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
     },
+    ---@module 'treesitter-modules'
+    ---@type ts.mod.UserConfig
+    opts = {
+      ensure_installed = { "lua" },
+      auto_install = true,
+      highlight = {
+        enable = true,
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+    branch = "main",
+    init = function()
+      vim.g.no_plugin_maps = true
+    end,
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "V", --"<c-v>", -- blockwise
+          },
+          include_surrounding_whitespace = false,
+        },
+      })
+
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@parameter.outer", "textobjects")
+      end, { desc = "around argument (textobject)" })
+      vim.keymap.set({ "x", "o" }, "ia", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@parameter.inner", "textobjects")
+      end, { desc = "inside argument (textobject)" })
+      vim.keymap.set({ "x", "o" }, "af", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@function.outer", "textobjects")
+      end, { desc = "around function (textobject)" })
+      vim.keymap.set({ "x", "o" }, "if", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@function.inner", "textobjects")
+      end, { desc = "inside function (textobject)" })
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@class.outer", "textobjects")
+      end, { desc = "around class (textobject)" })
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@class.inner", "textobjects")
+      end, { desc = "inside class (textobject)" })
+      vim.keymap.set({ "x", "o" }, "as", function()
+        require("nvim-treesitter-textobjects.select").select_textobject("@local.scope", "locals")
+      end, { desc = "around scope (textobject)" })
+    end,
   },
 }
